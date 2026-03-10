748bf92d0504d21c8a33678cf5061efaa21ba05d
VANHORNMA22 <vanhornma22@gcc.edu>
2026-03-10 15:09:50 -0400

    Created ProfessorFilterTest class and copied in the helper method created in R1d to makeSection. Also added the imports and first test

---
+++ backend/src/test/java/edu/gcc/gravy/ProfessorFilterTest.java
@@ -0,0 +1,32 @@
+package edu.gcc.gravy;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfessorFilterTest {
+
+    // Helper method to construct sections for testing.
+    private Section makeSection(int id, String title, String department, char letter) {
+
+        Course course = new Course(id, title, department, 3, "Fall");
+
+        Section section = new Section();
+        section.setCourse(course);
+        section.setSectionID(letter);
+
+        return section;
+    }
+
+    @Test
+    void getType() {
+
+        DepartmentFilter filter = new DepartmentFilter("COMP");
+
+        // The filter type should always be DEPARTMENT
+        assertEquals(FilterType.DEPARTMENT, filter.getType());
+    }
+
+}
