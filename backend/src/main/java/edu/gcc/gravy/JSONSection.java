3ce4aa0efa7ba8cd600fc53d43b94b6463e21571
youngLuke26 <YoungLT22@gcc.edu>
2026-03-10 14:41:05 -0400

    Generated JSONSection.java file and populated member variables.
    Created readJSON method in main to read from the data_wolfe.json file.

---
+++ backend/src/main/java/edu/gcc/gravy/JSONSection.java
@@ -0,0 +1,26 @@
+package edu.gcc.gravy;
+
+import java.util.List;
+
+public class JSONSection {
+    public int credits;
+    public String[] faculty;
+    public boolean is_lab;
+    public boolean is_open;
+    public String location;
+    public String name;
+    public int number;
+    public int open_seats;
+    public String section;
+    public String semester;
+    public String subject;
+    public List<time> times;
+    public int total_seats;
+
+
+    public static class time{
+        public String day;
+        public String end_time;
+        public String start_time;
+    }
+}
--- backend/src/main/java/edu/gcc/gravy/Main.java
+++ backend/src/main/java/edu/gcc/gravy/Main.java
@@ -1,10 +1,21 @@
 package edu.gcc.gravy;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 /**
  * @author: goettelsg24
  */
 
 public class Main {
+
+    public static ArrayList<Section> allSections;
+
     public static void main(String[] args) {
 
     }
@@ -15,6 +26,34 @@ public class Main {
         // Get all user I/O
         // Apply filters
         // Display results
+    }
+
+    public static void readJSON(){
+        String jsonPath = "./data_wolfe.json";
+        try (InputStream inputStream = Files.newInputStream(Path.of(jsonPath));
+             JsonReader reader = new JsonReader(new InputStreamReader(inputStream))
+             ) {
+            // Create Gson instance
+            Gson gson = new Gson();
+
+            ArrayList<Section> sections = new ArrayList<>();
+
+            reader.beginArray();
+            while (reader.hasNext()) {
+                sections.add(gson.fromJson(reader, JSONSection.class));
+            }
+            reader.endArray();
+
+            // Iterate and print the objects
+            for (Section section : sections) {
+                System.out.println(section);
+            }
+
+            allSections = sections;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
