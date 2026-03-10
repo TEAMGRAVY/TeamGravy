664fdb432c2966d167f594ca05efbb644dbe155e
VANHORNMA22 <vanhornma22@gcc.edu>
2026-03-10 12:35:24 -0400

    Generated the DepartmentFilterTest class and stub code.

---
+++ backend/src/test/java/edu/gcc/gravy/DepartmentFilterTest.java
@@ -0,0 +1,16 @@
+package edu.gcc.gravy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentFilterTest {
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void apply() {
+    }
+}
