5f059c6ce771a20e4ebe3cabfe2453d3695abfe2
youngLuke26 <YoungLT22@gcc.edu>
2026-03-11 10:54:16 -0400

    Add toTimeSlot method for the timesubclass in JSONSection.java
    Adjusted toSection method.

--- backend/src/main/java/edu/gcc/gravy/JSONSection.java
+++ backend/src/main/java/edu/gcc/gravy/JSONSection.java
@@ -1,6 +1,8 @@
 package edu.gcc.gravy;
 
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Set;
 
 public class JSONSection {
     public int credits;
@@ -18,14 +20,46 @@ public class JSONSection {
     public int total_seats;
 
 
-    public static class time{
+    public static class time {
         public String day;
         public String end_time;
         public String start_time;
+
+        public TimeSlot toTimeSlot(){
+            return new TimeSlot(LocalTime.parse(start_time), LocalTime.parse(end_time),
+                    switch (day) {
+                        case ("M"):
+                            yield Set.of(Day.MONDAY);
+                        case ("T"):
+                            yield Set.of(Day.TUESDAY);
+                        case ("W"):
+                            yield Set.of(Day.WEDNESDAY);
+                        case ("R"):
+                            yield Set.of(Day.THURSDAY);
+                        case ("F"):
+                            yield Set.of(Day.FRIDAY);
+                        default:
+                            yield Set.of();
+                    }
+
+
+
+                    );
+        }
+
     }
 
     public Section toSection(){
-        Section current = new Section();
+
+        for (time curr : times){
+
+        }
+
+        Course currentCourse = new Course(number, name, subject, credits, semester);
+        Section currentSection = new Section(currentCourse, section.charAt(0), faculty[0], total_seats, total_seats-open_seats,
+                ;
+
+        return currentSection;
     }
 
 }
