78a69a315b0f91e5e167ffdf3c4855362daf1448
VANHORNMA22 <vanhornma22@gcc.edu>
2026-03-17 11:07:48 -0400

    Added TermFilterTest

---
+++ backend/src/test/java/edu/gcc/gravy/TermFilterTest.java
@@ -0,0 +1,16 @@
+package edu.gcc.gravy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TermFilterTest {
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void apply() {
+    }
+}
