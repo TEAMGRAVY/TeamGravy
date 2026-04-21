56893284ccb898455a45474ea942158e6ebbde21
VANHORNMA22 <vanhornma22@gcc.edu>
2026-04-20 22:58:05 -0400

    Added new test file for OpenFilter called OpenFilterTest. Has not been populated yet.

---
+++ backend/src/test/java/edu/gcc/gravy/OpenFilterTest.java
@@ -0,0 +1,16 @@
+package edu.gcc.gravy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenFilterTest {
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void apply() {
+    }
+}
