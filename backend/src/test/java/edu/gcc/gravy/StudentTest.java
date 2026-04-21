c949cbec130ec54d80fd29cece7f90d92e7d731d
VANHORNMA22 <vanhornma22@gcc.edu>
2026-04-20 22:52:22 -0400

    Created a StudentTest file to account for an oversight in the Student class's testing.

---
+++ backend/src/test/java/edu/gcc/gravy/StudentTest.java
@@ -0,0 +1,40 @@
+package edu.gcc.gravy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void createSchedule() {
+    }
+
+    @Test
+    void removeSchedule() {
+    }
+
+    @Test
+    void addCompletedCourses() {
+    }
+
+    @Test
+    void getSchedules() {
+    }
+
+    @Test
+    void getYear() {
+    }
+
+    @Test
+    void getMajor() {
+    }
+
+    @Test
+    void getMinor() {
+    }
+
+    @Test
+    void getCompletedCourses() {
+    }
+}
