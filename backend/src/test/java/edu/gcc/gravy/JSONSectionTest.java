f595af6b606a59c335cac8158c6d12dc448526c9
VANHORNMA22 <vanhornma22@gcc.edu>
2026-04-20 23:06:54 -0400

    Created a test file for JSONSection, as there were missed methods and lines from other tests.

---
+++ backend/src/test/java/edu/gcc/gravy/JSONSectionTest.java
@@ -0,0 +1,12 @@
+package edu.gcc.gravy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JSONSectionTest {
+
+    @Test
+    void toSection() {
+    }
+}
