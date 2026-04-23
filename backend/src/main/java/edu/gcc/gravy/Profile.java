eab3a36126c2afc3d435974d142347336538368e
youngLuke26 <YoungLT22@gcc.edu>
2026-04-22 23:35:48 -0400

    Made changes to save the Profiles as JSON object. Incorporated a HashUtil class to use SHA256 hashing for passwords on profiles.

--- backend/src/main/java/edu/gcc/gravy/Profile.java
+++ backend/src/main/java/edu/gcc/gravy/Profile.java
@@ -1,23 +1,90 @@
 package edu.gcc.gravy;
 
+import java.awt.*;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Profile {
     private String name;
+    private String hashedPassword;
     private int gradYear;
     private String major;
-    private List<Course> coursesTaken;
+    //private List<Course> coursesTaken;
+    private preferences preferences;
 
-    void setName(String name){
+    public void setName(String name){
         this.name = name;
     }
 
-    void setMajor(String major){
+    public String getName(){
+        return name;
+    }
+
+    public void setMajor(String major){
         this.major = major;
     }
 
-    void setGradYear(int gradYear){
+    public String getMajor(){
+        return major;
+    }
+
+    public void setGradYear(int gradYear){
         this.gradYear = gradYear;
+    }
+
+    public int getGradYear(){
+        return gradYear;
+    }
+
+    public void setHashedPassword(String password) throws NoSuchAlgorithmException {
+        this.hashedPassword = HashUtils.sha256(password);
+    }
+
+    public String getHashedPassword(){
+        return hashedPassword;
+    }
+
+//    public void addCourse(Course addition){
+//        if (coursesTaken == null){
+//            coursesTaken = new ArrayList<>();
+//        }
+//        coursesTaken.add(addition);
+//    }
+
+//    public String removeCourse(Course removal){
+//        if (coursesTaken == null){
+//            return "No courses in list";
+//        }
+//        return (coursesTaken.remove(removal)) ? "Success" : "Course not in list";
+//    }
+//
+//    public List<Course> getCoursesTaken() {
+//        return coursesTaken;
+//    }
+
+    public void setPreferences(boolean darkMode, boolean longestBreak, boolean showWarnings){
+        if (preferences == null){
+            preferences = new preferences();
+        }
+        preferences.darkMode = darkMode;
+        preferences.longestBreak = longestBreak;
+        preferences.showWarnings = showWarnings;
+    }
+
+    public boolean[] getPreferences() {
+        return new boolean[]{
+                preferences.darkMode,
+                preferences.longestBreak,
+                preferences.showWarnings
+        };
+    }
+
+    private static class preferences {
+        public preferences(){}
+        public boolean darkMode;
+        public boolean longestBreak;
+        public boolean showWarnings;
     }
 }
